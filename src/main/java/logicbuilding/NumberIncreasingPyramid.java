2a2a1e3f1817b5e7c2cd754f67f7daf514d1cd0c
Saurabh Manmode <saurabh.manmode94@gmail.com>
2025-10-01 00:09:50 +0530

    29/09/2025 NumberChangingPyramid.java, NumberIncreasingPyramid, NumberIncreasingReversePyramid

---
+++ src/main/java/logicbuilding/NumberIncreasingPyramid.java
@@ -0,0 +1,20 @@
+package logicbuilding;
+
+public class NumberIncreasingPyramid {
+
+    public static void main(String[] args) {
+        int n = 4;
+        printNumberIncreasingPyramid(n);
+    }
+
+    public static void printNumberIncreasingPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
