442d3089ea2f384cede97a56c98052a1218b79c1
Saurabh Manmode <saurabh.manmode94@gmail.com>
2025-12-28 21:14:45 +0530

    add remove duplicates using hashset and java 8

--- src/main/java/org/example/array/RemoveDuplicatesFromArray.java
+++ src/main/java/org/example/array/RemoveDuplicatesFromArray.java
@@ -1,5 +1,11 @@
 package org.example.array;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class RemoveDuplicatesFromArray {
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6};
@@ -8,6 +14,29 @@ public class RemoveDuplicatesFromArray {
         for (var i = 0; i < newLength; i++) {
             System.out.println(arr[i]);
         }
+
+        //using hashset
+        Set<Integer> distinct = findDistinctElement(arr);
+        System.out.println(distinct);
+
+        //using java 8
+
+        List<Integer> distinctVal = Arrays.stream(arr)
+                .distinct()
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(distinctVal);
+
+    }
+
+    private static Set<Integer> findDistinctElement(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (var i = 0; i < arr.length; i++) {
+            if (!set.contains(arr[i])) {
+                set.add(arr[i]);
+            }
+        }
+        return set;
     }
 
     private static int findNewLength(int[] arr) {
