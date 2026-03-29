826d94ac635ad48b5561c6c5c940a7c7eec6c2af
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-03-29 23:09:42 +0530

    Strings: length of string

---
+++ src/main/java/org/example/strings/LengthOfString.java
@@ -0,0 +1,8 @@
+package org.example.strings;
+
+public class LengthOfString {
+    public static void main(String[] args) {
+        String s = "Saurabh";
+        System.out.println(s.length());
+    }
+}
