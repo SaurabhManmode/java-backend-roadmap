2efc115241c78b8c5b45139376d09c75000357bd
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-04-01 23:33:36 +0530

    Array: sorted array or not

---
+++ src/main/java/org/example/array/striver/ArraySortedOrNot.java
@@ -0,0 +1,18 @@
+package org.example.array.striver;
+
+public class ArraySortedOrNot {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 4, 5, 6};
+        System.out.println(isSorted(arr));
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
