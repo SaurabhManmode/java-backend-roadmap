ef70e049b8a349cc71a50ca9e19ac02d081961e8
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-04-21 13:32:38 +0530

    add : solution with O(1) complexity

--- src/main/java/org/example/array/striver/LeftRotateArrayByD.java
+++ src/main/java/org/example/array/striver/LeftRotateArrayByD.java
@@ -8,10 +8,28 @@ public class LeftRotateArrayByD {
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
         int d = 3;
-
+        int n = arr.length;
+        //having space complexity
         rotateArrayByD(arr, d);
         System.out.println(Arrays.toString(arr));
 
+        //avoid space
+        reverseArray(arr, 0, d - 1);
+        reverseArray(arr, d, n - 1);
+        reverseArray(arr, 0, n - 1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void reverseArray(int[] arr, int start, int end) {
+
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     private static void rotateArrayByD(int[] arr, int d) {
@@ -32,4 +50,9 @@ public class LeftRotateArrayByD {
         }
 
     }
+
+
+    //avoiding space
+
+
 }
