3f3e52150b2906f0b3cb06c87553a44ccccf4f9d
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-04-23 00:41:56 +0530

    overriding in value in finally block

--- src/main/java/org/example/exceptions/TryCatchBasics.java
+++ src/main/java/org/example/exceptions/TryCatchBasics.java
@@ -5,11 +5,20 @@ public class TryCatchBasics {
     public static void main(String[] args) {
 
         // skipping finally
-        try{
-            System.exit(0);
-        }finally {
+        try {
+          //  System.exit(0);
+        } finally {
             System.out.println("Hello");
         }
+        //values in finally
+        System.out.println(overridingValuesInFinally());
+    }
 
+    static int overridingValuesInFinally() {
+        try {
+            return 1;
+        } finally {
+            return 2;
+        }
     }
 }
