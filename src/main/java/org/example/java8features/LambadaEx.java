d68c660e8b65c180fb7fa70f975971d8a921be3c
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-01 12:30:17 +0530

    lambada with multithreaading added

--- src/main/java/org/example/java8features/LambadaEx.java
+++ src/main/java/org/example/java8features/LambadaEx.java
@@ -3,9 +3,17 @@ package org.example.java8features;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class LambadaEx {
+
+    static String[] games
+            = {"COD", "Prince Of Persia", "GTA-V5",
+            "Valorant", "FIFA 22", "Fortnite"};
+    static String[] tracks
+            = {"Believer", "Cradles", "Taki Taki", "Sorry",
+            "Let Me Love You"};
 
     public static void main(String[] args) {
         // how we can provide implementation for the lambada expression
@@ -53,12 +61,52 @@ public class LambadaEx {
 
         // creation of thread using lambada
         Runnable myThread = () -> {
-            Thread.currentThread().setName("Saurabh's Thread");
+            Thread.currentThread().setName("Saurabh's Thread"); // thread 0
             System.out.println(Thread.currentThread().getName());
         };
 
         Thread thread = new Thread(myThread);
         thread.start();
+
+        //multi threading example 2
+        Runnable basic = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Running task by the " + threadName);
+
+        };
+
+        Thread basic1 = new Thread(basic);
+        Thread basic2 = new Thread(basic);
+        basic1.start();
+        basic2.start();
+
+        //lambada with multithreading example 2
+        RandomPlayer randomPlayer = new RandomPlayer();
+
+        Random random = new Random();
+
+        Runnable gameRunner = () -> {
+            try {
+
+                randomPlayer.playGame(games[random.nextInt(games.length)]);
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
+        };
+
+        Runnable musicPlayer = () -> {
+            try {
+                randomPlayer.playMusic(tracks[random.nextInt(tracks.length)]);
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
+        };
+
+        Thread game = new Thread(gameRunner);
+        Thread music = new Thread(musicPlayer);
+
+        game.start();
+        music.start();
 
     }
 }
@@ -78,4 +126,18 @@ interface NoParameter {
 
 interface SingleParameter {
     int square(int a);
+}
+
+class RandomPlayer {
+    public void playGame(String gameName) throws InterruptedException {
+        System.out.println(gameName + " game started");
+        Thread.sleep(500);
+        System.out.println("game ended");
+    }
+
+    public void playMusic(String trackName) throws InterruptedException {
+        System.out.println(trackName + " is playing");
+        Thread.sleep(500);
+        System.out.println("music ended");
+    }
 }
