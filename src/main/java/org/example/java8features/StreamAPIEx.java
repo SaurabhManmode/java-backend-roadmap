be47777b3838ebddb881ee1a1da97e99d63e787f
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-05 22:16:35 +0530

    filter operation

--- src/main/java/org/example/java8features/StreamAPIEx.java
+++ src/main/java/org/example/java8features/StreamAPIEx.java
@@ -2,6 +2,8 @@ package org.example.java8features;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -17,7 +19,7 @@ public class StreamAPIEx {
         Stream<Integer> integerStream = Stream.of(1, 2, 3);
 
         //from array
-        int [] arr ={1,2,4,5,6};
+        int[] arr = {1, 2, 4, 5, 6};
         IntStream fromArray = Arrays.stream(arr);
 
         //create empty stream
@@ -26,8 +28,31 @@ public class StreamAPIEx {
         //using builder
         Stream.Builder<Object> builder = Stream.builder();
 
+        //Intermediate operations
+        //1. Filter
+        List<Integer> ls = List.of(5, 10, 27, 4, 80, 60);
+        List<Integer> filteredlist = ls.stream()
+                .filter(n -> n % 5 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(filteredlist);
+        Stream<String> stream = Stream.of(
+                "Geeks", "fOr", "GEEKSQUIZ", "GeeksforGeeks");
+
+      /*  List<String> capitalStrings = stream.filter(str -> Character.isUpperCase(str.charAt(1)))
+                .collect(Collectors.toList());*/
+
+   //     System.out.println(capitalStrings);
+
+        List<String> endsWithS = stream.filter(
+                str -> str.endsWith("s")
+        ).collect(Collectors.toList());
+
+        System.out.println(endsWithS);
 
     }
+
+
 }
 
 
