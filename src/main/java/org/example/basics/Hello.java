d00da3464f782039a463440c05d6cb363a259658
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-06-01 23:23:39 +0530

    Hello world programme

---
+++ src/main/java/org/example/basics/Hello.java
@@ -0,0 +1,10 @@
+package org.example.basics;
+
+import java.sql.SQLOutput;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
